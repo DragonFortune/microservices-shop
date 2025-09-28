2ba327c5c039742893af02ca5b6f1f1e6405982b
Кристина <dragonfartune@gmail.com>
2025-09-28 22:32:32 +0300

    feat: начало работы с ошибками

---
+++ order-service/src/main/java/com/example/orderservice/exception/ErrorHandlerAdvice.java
@@ -0,0 +1,24 @@
+package com.example.orderservice.exception;
+
+import com.example.orderservice.exception.types.NotFoundException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+@RestControllerAdvice
+@Slf4j(topic = "ErrorHandlerAdvice")
+public class ErrorHandlerAdvice {
+
+    @ResponseStatus(NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ErrorResponse handleNotFoundException(NotFoundException exception) {
+        log.error("404 {}", exception.getMessage());
+        return ErrorResponse.builder()
+                .errorCode(NOT_FOUND.value())
+                .errorMessage(exception.getMessage())
+                .build();
+    }
+}
